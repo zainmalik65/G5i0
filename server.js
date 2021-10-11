3914f0769f93f802d7e9d332c8960f3ace1bd53c
Your Name <you@example.com>
2021-10-12 03:47:26 +0500

    first commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/coursel'));
+app.use(express.static('./dist/cour'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/coursel/'}),
+    res.sendFile('index.html', {root: 'dist/cour/'}),
 );
 
 // Start the app by listening on the default Heroku port
